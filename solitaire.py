bc998dbb62bbbca8de36309e33b4058d0e2604e6
Daniel Nkunga <daniel.s.nkunga@uark.edu>
2025-10-26 09:51:01 -0500

    Initial game state with no play logic
    Needs a better print

--- solitaire.py
+++ solitaire.py
@@ -4,6 +4,7 @@
     Description: Creating solitaire game
 """
 
+import random
 from card import Card, Suit
 
 class SolitaireCard(Card):
@@ -23,16 +24,66 @@ class SolitaireCard(Card):
 class Solitaire:
     def __init__(self):
         print("New solitaire game!")
-        self.deck = self._createDeck()
+
+        # Creating the deck
+        self._deck = self._createDeck()
+        random.shuffle(self._deck)
+
+        # Create parts of the game
+
+        # Foundations
+        self.heart_foundation = []
+        self.diamond_foundation = []
+        self.club_foundation = []
+        self.spade_foundation = []
+
+        # Tableau
+        self.tableau = [[],[],[],[],[],[],[]]
+
+        # Draw/Discard
+        self.stock = []
+        self.waste = []
+
+        # Initialize game
+
+        # Tableau
+        self._initialize_tableau()
+        self._print_tableau()
+
+        # Stock
+        for card in range(self._deck.__len__()):
+            self.stock.append(self._deck.pop())
+
+        for card in self._deck:
+            print(card)
+        print(self.stock.__len__())
+        print(self._deck.__len__())
         
 
     def _createDeck(self):
         temp = []
         for suit in Suit:
-            for value in range(13):
+            for value in range(1,14):
                 card = SolitaireCard(suit, value)
                 temp.append(card)
         return temp
+
+    def _initialize_tableau(self):
+        for col in range(7):
+            for row in range(col + 1):
+                card = self._deck.pop()
+                # Only the top card in each column is visible
+                if row == col:
+                    card.isHidden = False
+                self.tableau[col].append(card)
+
+    def _print_tableau(self):
+        """Helper function to print out the tableau for testing."""
+        print("\nTableau Layout:")
+        for i, col in enumerate(self.tableau):
+            cards = " ".join(str(card) for card in col)
+            print(f"Column {i+1}: {cards}")
+
 
 def main():
     game = Solitaire()
